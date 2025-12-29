695c6f148297545d967e3dc5679b5e07046f705c
Quincy Milloy <qmilloy@mac.com>
2025-12-29 16:34:44 +0100

    Add more consistent song example and add error messaging for when song is not recognized.

--- examples/recognize-song.rs
+++ examples/recognize-song.rs
@@ -4,16 +4,16 @@ use shazamrs::{Shazam, ShazamError};
 async fn main() -> Result<(), ShazamError> {
     let shazam = Shazam::with_segment_duration(5);
 
-    let response = shazam
-        .recognize_path("examples/data/MidnightSpecial.mp3")
-        .await?;
+    let response = shazam.recognize_path("examples/data/Gloria.ogg").await?;
 
     if let Some(track) = response.track {
         println!(
             "🎵 {} — {}",
             track.title.unwrap_or_default(),
-            track.subtitle.unwrap_or_default()
+            track.subtitle.unwrap_or_default(),
         );
+    } else {
+        println!("Got response but there was no track: {:?}", response);
     }
 
     Ok(())
